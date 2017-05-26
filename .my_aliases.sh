a0e5c7a8e0ff11183bbfee57f3b0015f55b78666
gk0909c <gk0909c@gmail.com>
2017-05-26 11:07:28 +0900

    add alias about bundle

--- .my_aliases.sh
+++ .my_aliases.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# about bundle
+alias be-rake="bundle exec rake "
+alias be-rails="bundle exec rails "
+alias be-rspec="bundle exec rspec "
+
 # about rails
 alias rails-server="bundle exec rails server -b 0.0.0.0"
+
 # use light server
 alias publish-istanbul="light-server -s ./coverage/lcov-report -p 3000 -b 0.0.0.0"
 alias publish-jsdoc="light-server -s ./jsdoc -p 3000 -b 0.0.0.0"
