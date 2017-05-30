ef95c7514c960a08aaf894910391f36595c0f61d
gk0909c <gk0909c@gmail.com>
2017-05-30 10:23:02 +0900

    add ctags alias

--- .my_aliases.sh
+++ .my_aliases.sh
@@ -12,3 +12,6 @@ alias rails-server="bundle exec rails server -b 0.0.0.0"
 alias publish-istanbul="light-server -s ./coverage/lcov-report -p 3000 -b 0.0.0.0"
 alias publish-jsdoc="light-server -s ./jsdoc -p 3000 -b 0.0.0.0"
 alias publish-simplecov="light-server -s ./coverage -p 3000 -b 0.0.0.0"
+
+# ctags
+alias ctags-gen-rails='ctags --langmap=RUBY:.rb --exclude="*.js"  --exclude=".git*" -R'
