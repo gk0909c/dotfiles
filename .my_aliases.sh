51eb36308f216ece63e73fd0a205a3a0ed09a902
gk0909c <gk0909c@gmail.com>
2017-06-21 14:57:27 +0900

    change bash alias about rails

--- .my_aliases.sh
+++ .my_aliases.sh
@@ -6,7 +6,9 @@ alias be-rails="bundle exec rails "
 alias be-rspec="bundle exec rspec "
 
 # about rails
-alias rails-server="bundle exec rails server -b 0.0.0.0"
+alias rails-server="bundle exec rails server -b 0.0.0.0 -d"
+alias rails-server-f="bundle exec rails server -b 0.0.0.0"
+alias kill-rails-server='kill `cat tmp/pids/server.pid`'
 
 # use light server
 alias publish-istanbul="light-server -s ./coverage/lcov-report -p 3000 -b 0.0.0.0"
